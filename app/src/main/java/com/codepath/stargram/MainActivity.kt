7e84538027ebadeafb5656a518b74e0f0161cb0a
Melissa Perez <melmperz@gmail.com>
2023-04-13 20:29:51 -0400

    Initial commit

---
+++ app/src/main/java/com/codepath/stargram/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.codepath.stargram
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * Let user create a post by taking a photo with their camera
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
