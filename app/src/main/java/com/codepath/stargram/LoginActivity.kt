9b2e46bf6202d0a658e090daf62f97ddb8bb649a
Melissa Perez <melmperz@gmail.com>
2023-04-14 02:33:51 -0400

    Remove API information

--- app/src/main/java/com/codepath/stargram/LoginActivity.kt
+++ app/src/main/java/com/codepath/stargram/LoginActivity.kt
@@ -1,11 +1,83 @@
 package com.codepath.stargram
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.parse.ParseUser
 
 class LoginActivity : AppCompatActivity() {
+    private val tag = "LoginActivity/"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        // check if user is logged in -> go to main activity instead
+        if (ParseUser.getCurrentUser() != null) {
+            goToMainActivity()
+        }
+
+        findViewById<Button>(R.id.login_button).setOnClickListener {
+            val username: String = findViewById<EditText>(R.id.stargram_username).text.toString()
+            val password: String = findViewById<EditText>(R.id.stargram_password).text.toString()
+
+            loginUser(username, password)
+        }
+
+        findViewById<Button>(R.id.signup_button).setOnClickListener {
+            val username: String = findViewById<EditText>(R.id.stargram_username).text.toString()
+            val password: String = findViewById<EditText>(R.id.stargram_password).text.toString()
+
+            signUpUser(username, password)
+        }
+
+    }
+
+    private fun signUpUser(username: String, password: String) {
+        val user = ParseUser()
+
+        user.username = username
+        user.setPassword(password)
+
+        user.signUpInBackground { e ->
+            if (e == null) {
+                // Hooray! Let them use the app now.
+                //TODO: user to main show a toast
+                Log.i(tag, "Successfully signed up user")
+                goToMainActivity()
+
+            } else {
+                e.printStackTrace()
+                Toast.makeText(this, "Error sign up user", Toast.LENGTH_SHORT).show()
+
+            }
+        }
+    }
+
+    private fun loginUser(username: String, password: String) {
+        ParseUser.logInInBackground(
+            username, password, ({ user, e ->
+                if (user != null) {
+                    Log.i(tag, "Successfully logged in user")
+                    goToMainActivity()
+                } else {
+                    e.printStackTrace()
+                    Toast.makeText(this, "Error login in user", Toast.LENGTH_SHORT).show()
+                }
+            })
+        )
+    }
+
+    private fun goToMainActivity() {
+        val intent = Intent(this@LoginActivity, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+        // need a log out button
+        //ParseUser.logOut()
+        //val currentUser = ParseUser.getCurrentUser()
     }
 }
---
+++ app/src/main/java/com/codepath/stargram/StargramApplication.kt
@@ -0,0 +1,30 @@
+package com.codepath.stargram
+
+import android.app.Application
+import com.parse.Parse
+import com.parse.ParseObject
+import okhttp3.OkHttpClient
+
+private val CLIENT_KEY = BuildConfig.CLIENT_KEY
+private val API_ID = BuildConfig.API_ID
+private val SERVER_URL = BuildConfig.SERVER_URL
+
+class StargramApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Parse.setLogLevel(Parse.LOG_LEVEL_DEBUG)
+
+        Parse.initialize(
+            Parse.Configuration.Builder(this)
+                .applicationId(API_ID)
+                .clientKey(CLIENT_KEY)
+                .server(SERVER_URL).build())
+
+        // New test creation of object below
+        //val testObject = ParseObject("TestObject")
+        //testObject.put("foo", "bar")
+        //testObject.saveInBackground()
+
+        // push notification if needed here
+    }
+}
